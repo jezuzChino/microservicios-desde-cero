3049b66d93d023ab3881a641f8c9e7a2e25699a9
jezuzChino <jesus.chino.r@gmail.com>
2025-08-17 13:11:25 -0600

    init

---
+++ artifact-demo01/artifact-demo01/src/main/java/com/primer/micro/servicio/artifact_demo01/NameArtifactDemo01Application.java
@@ -0,0 +1,13 @@
+package com.primer.micro.servicio.artifact_demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameArtifactDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameArtifactDemo01Application.class, args);
+	}
+
+}
---
+++ artifact-demo01/artifact-demo01/src/test/java/com/primer/micro/servicio/artifact_demo01/NameArtifactDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primer.micro.servicio.artifact_demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameArtifactDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
