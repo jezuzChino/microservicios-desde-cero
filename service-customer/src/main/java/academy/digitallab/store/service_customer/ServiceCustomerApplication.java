f812e1bfed7ed892f58f37f990a6e56f14866dfd
jezuzChino <jesus.chino.r@gmail.com>
2025-09-01 20:24:25 -0600

    feat add config to registration eureka server

--- service-customer/src/main/java/academy/digitallab/store/service_customer/ServiceCustomerApplication.java
+++ service-customer/src/main/java/academy/digitallab/store/service_customer/ServiceCustomerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableEurekaClient en versiones recientes no es necesario solo con agregar la dependencia de Eureka Client es suficiente
 public class ServiceCustomerApplication {
 
 	public static void main(String[] args) {
