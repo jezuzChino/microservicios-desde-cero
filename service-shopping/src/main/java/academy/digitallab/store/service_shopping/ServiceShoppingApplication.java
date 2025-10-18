277ff7f1adfdb671d93e9f0c1d4969798960c2f3
jezuzChino <jesus.chino.r@gmail.com>
2025-10-17 21:12:26 -0600

    feat add enabled feignCleint

--- service-shopping/src/main/java/academy/digitallab/store/service_shopping/ServiceShoppingApplication.java
+++ service-shopping/src/main/java/academy/digitallab/store/service_shopping/ServiceShoppingApplication.java
@@ -2,9 +2,11 @@ package academy.digitallab.store.service_shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@EnableEurekaClient en versiones recientes no es necesario solo con agregar la dependencia de Eureka Client es suficiente
 @SpringBootApplication
+@EnableFeignClients // Habilita los clientes Feign en la aplicación
 public class ServiceShoppingApplication {
 
 	public static void main(String[] args) {
