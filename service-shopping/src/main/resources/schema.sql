7f90647828e674a47ef47e4caf3a245f22d1c0a3
jezuzChino <jesus.chino.r@gmail.com>
2025-10-17 20:16:43 -0600

    feat add properties to enabled/dissabled eureka and change order schema.sql

--- service-shopping/src/main/resources/schema.sql
+++ service-shopping/src/main/resources/schema.sql
@@ -1,12 +1,3 @@
-DROP TABLE IF EXISTS tbl_invoice_items;
-CREATE TABLE tbl_invoice_items (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    invoice_id BIGINT NOT NULL,
-    product_id BIGINT NOT NULL,
-    quantity INTEGER NOT NULL,
-    price DOUBLE NOT NULL
-);
-
 DROP TABLE IF EXISTS tbl_invoices;
 CREATE TABLE tbl_invoices (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -15,4 +6,13 @@ CREATE TABLE tbl_invoices (
     customer_id BIGINT NOT NULL,
     create_at TIMESTAMP NOT NULL,
     state VARCHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS tbl_invoice_items;
+CREATE TABLE tbl_invoice_items (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    invoice_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INTEGER NOT NULL,
+    price DOUBLE NOT NULL
 );
